bcf1dd7806c83f5a66b4c23da4b612b03c0aa7d5
bulutciftci <bulutciftci1881@gmail.com>
2024-08-06 21:11:52 +0300

    2. adım kodları

---
+++ src/layout/main/sidebar/menu/new/index.jsx
@@ -0,0 +1,9 @@
+import Button from "~/components/button/index.jsx";
+
+export default function New() {
+    return (
+        <div className="py-4 w-[90%]">
+            <Button size="large">Gönder</Button>
+        </div>
+    )
+}
